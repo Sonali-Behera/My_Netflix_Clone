9a412bd182dd99d671b04de23edfb0da201d3e2e
*Sonali <*sonalibehera.bbsr@gmail.com*>
2022-07-01 14:20:54 +0530

    Do basic set-up before starting to develop the project

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBT-PLizr40wqRaJCqjKHPPsNwi3bGPO3o",
+    authDomain: "my-netflix-clone-68997.firebaseapp.com",
+    projectId: "my-netflix-clone-68997",
+    storageBucket: "my-netflix-clone-68997.appspot.com",
+    messagingSenderId: "932381888432",
+    appId: "1:932381888432:web:9b00d07144e7ee6c7c5075"
+  };
